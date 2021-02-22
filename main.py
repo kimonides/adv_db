6e1c0a02cc68c3d1df6cc783b4cfcc94543324f6
kimonides <kimonide@gmail.com>
2021-02-22 11:17:54 +0200

    finished q3

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from pyspark.sql import Row
 from io import StringIO
 import csv
 
+
 def split_complex(x):
-        return list(csv.reader(StringIO(x),delimiter=','))[0]
+    return list(csv.reader(StringIO(x), delimiter=','))[0]
+
 
 spark = SparkSession.builder.appName("paconator").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
@@ -25,33 +27,33 @@ sc = spark.sparkContext
 
 # ---------------------------------------------                 Query 1               ------------------------------------------------
 
-def getEarnings(a,b):
-        print("a is:" + a)
-        print("b is:" + b)
-        a = int(a)
-        b = int(b)
-        return (float(a)-float(b))/float(b)
-        
-
-q1 =    sc.textFile('hdfs://master:9000/data/movies.csv'). \
-        map(lambda row : split_complex(row)). \
-        filter(lambda row: False if row[3] == None else True). \
-        filter(lambda row: True if row[3].split('-')[0]>='2000' and row[3].split('-')[0].isdigit() else False ). \
-        filter(lambda row: True if row[6] > '0' and row[5] > '0' else False ). \
-        map(lambda row: ( row[3].split('-')[0], (row[1], getEarnings(row[6],row[5])  ) ) ). \
-        reduceByKey(lambda x, y : x if x[1]>y[1] else y  ). \
-        sortByKey(ascending=False). \
-        take(20)
+# def getEarnings(a,b):
+#         print("a is:" + a)
+#         print("b is:" + b)
+#         a = int(a)
+#         b = int(b)
+#         return (float(a)-float(b))/float(b)
 
 
-for i in q1:
-        print(i)
+# q1 =    sc.textFile('hdfs://master:9000/data/movies.csv'). \
+#         map(lambda row : split_complex(row)). \
+#         filter(lambda row: False if row[3] == None else True). \
+#         filter(lambda row: True if row[3].split('-')[0]>='2000' and row[3].split('-')[0].isdigit() else False ). \
+#         filter(lambda row: True if row[6] > '0' and row[5] > '0' else False ). \
+#         map(lambda row: ( row[3].split('-')[0], (row[1], getEarnings(row[6],row[5])  ) ) ). \
+#         reduceByKey(lambda x, y : x if x[1]>y[1] else y  ). \
+#         sortByKey(ascending=False). \
+#         take(20)
+
+
+# for i in q1:
+#         print(i)
 
 # ---------------------------------------------                 Query 2               ------------------------------------------------
 
-# spark.read.parquet('hdfs://master:9000/data/ratings.parquet').rdd. \
 # q2 =    sc.textFile('hdfs://master:9000/data/ratings.csv'). \
-#         map(lambda row:  ( row.split(',')[0]  , (float(row.split(',')[2]), 1 ) )  ). \
+#         map(lambda row: split_complex(row)). \
+#         map(lambda row:  ( row[0]  , (float(row[2]), 1 ) )  ). \
 #         reduceByKey( lambda x,y : ( x[0]+y[0] , x[1] + y[1] ) ). \
 #         map( lambda row : (row[0] ,( row[1][0]/row[1][1]) )     ). \
 #         map( lambda row : (None,(1,1)) if row[1]>3 else (None,(0,1)) ). \
@@ -65,20 +67,24 @@ for i in q1:
 # ---------------------------------------------                 Query 3               ------------------------------------------------
 
 
-# ratings =       sc.textFile('hdfs://master:9000/data/ratings.csv'). \
-#                 map(lambda row : (row.split(',')[1] , row.split(',')[2] )  )
+ratings = sc.textFile('hdfs://master:9000/data/ratings.csv'). \
+    map(lambda row: split_complex(row)). \
+    map(lambda row: (row[1], row[2]))
 
-# moviesGenres =          sc.textFile('hdfs://master:9000/data/movie_genres.csv'). \
-#                         map(lambda row : (row.split(',')[0] , row.split(',')[1] )  )
+moviesGenres = sc.textFile('hdfs://master:9000/data/movie_genres.csv'). \
+    map(lambda row: split_complex(row)). \
+    map(lambda row: (row[0], row[1]))
 
-# movieGenreRatings = ratings.join(moviesGenres)
+movieGenreRatings = ratings.join(moviesGenres)
+# Now we have tuples of ( movieID , (movieRating,movieGenre) )
 
-# q3 =    movieGenreRatings. \
-#         map( lambda row: (row[1][1] , row[1][0] ) ).collect()
-# for i in q3:
-#         print(i)
+q3 = movieGenreRatings. \
+        map(lambda row: (row[0], (float(row[1][0]), row[1][1], 1))). \
+        reduceByKey(lambda x, y: (x[0]+y[0], x[1], x[2]+y[2])). \
+        map( lambda row: ( row[1][1] , (row[1][0]/row[1][2] , 1 )) ). \
+        reduceByKey( lambda x,y:  (x[0]+y[0] , x[1]+y[1])). \
+        map( lambda row: ( row[0] , ( row[1][0]/row[1][1] , row[1][1] ) )).collect()
 
-# print(x.collect())
-
-
+for i in q3:
+    print(i)
 
