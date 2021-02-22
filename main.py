abac386354272ccdc397f17e640118eab917577e
kimonides <kimonide@gmail.com>
2021-02-22 17:57:55 +0200

    tired, still working on sql q1

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ sc = spark.sparkContext
 
 
 # q1 =    sc.textFile('hdfs://master:9000/data/movies.csv'). \
-#         map(lambda row : split_complex(row)). \
+#         map(split_complex). \
 #         filter(lambda row: False if row[3] == None else True). \
 #         filter(lambda row: True if row[3].split('-')[0]>='2000' and row[3].split('-')[0].isdigit() else False ). \
 #         filter(lambda row: True if row[6] > '0' and row[5] > '0' else False ). \
@@ -52,7 +52,7 @@ sc = spark.sparkContext
 # ---------------------------------------------                 Query 2               ------------------------------------------------
 
 # q2 =    sc.textFile('hdfs://master:9000/data/ratings.csv'). \
-#         map(lambda row: split_complex(row)). \
+#         map(split_complex). \
 #         map(lambda row:  ( row[0]  , (float(row[2]), 1 ) )  ). \
 #         reduceByKey( lambda x,y : ( x[0]+y[0] , x[1] + y[1] ) ). \
 #         map( lambda row : (row[0] ,( row[1][0]/row[1][1]) )     ). \
@@ -68,11 +68,11 @@ sc = spark.sparkContext
 
 
 # ratings = sc.textFile('hdfs://master:9000/data/ratings.csv'). \
-#     map(lambda row: split_complex(row)). \
+#     map(split_complex)). \
 #     map(lambda row: (row[1], row[2]))
 
 # moviesGenres = sc.textFile('hdfs://master:9000/data/movie_genres.csv'). \
-#     map(lambda row: split_complex(row)). \
+#     map(split_complex). \
 #     map(lambda row: (row[0], row[1]))
 
 # movieGenreRatings = ratings.join(moviesGenres)
@@ -90,36 +90,70 @@ sc = spark.sparkContext
 
 # ---------------------------------------------                 Query 4               ------------------------------------------------
 
-moviesGenres = sc.textFile('hdfs://master:9000/data/movie_genres.csv'). \
-        map(lambda row: split_complex(row)). \
-        filter(lambda row: True if row[1]=='Drama' else False ). \
-        map(lambda row: (row[0], row[1]))
+# moviesGenres = sc.textFile('hdfs://master:9000/data/movie_genres.csv'). \
+#         map(split_complex). \
+#         filter(lambda row: True if row[1]=='Drama' else False ). \
+#         map(lambda row: (row[0], row[1]))
 
-movies =        sc.textFile('hdfs://master:9000/data/movies.csv'). \
-                map( split_complex ). \
-                map( lambda row: ( row[0], (len(row[2]) , row[3].split('-')[0] ) ) ). \
-                filter( lambda row: True if row[1][1]>'1999' else False ). \
-                map( lambda row: ( row[0], ( row[1][0], (int(row[1][1]) % 100)//5 ) ) )
+# movies =        sc.textFile('hdfs://master:9000/data/movies.csv'). \
+#                 map( split_complex ). \
+#                 map( lambda row: ( row[0], (len(row[2]) , row[3].split('-')[0] ) ) ). \
+#                 filter( lambda row: True if row[1][1]>'1999' else False ). \
+#                 map( lambda row: ( row[0], ( row[1][0], (int(row[1][1]) % 100)//5 ) ) )
                 
-GenreLengths = moviesGenres.join(movies)
+# GenreLengths = moviesGenres.join(movies)
 
-q4 =    GenreLengths. \
-        map( lambda row: ( row[1][1][1],(row[1][1][0],1) ) ). \
-        reduceByKey( lambda x,y: (x[0]+y[0] , x[1]+y[1]) ). \
-        map( lambda row: ( row[0] , row[1][0]/row[1][1] ) ).collect()
-
-
-
-for i in q4:
-        print(i)
+# q4 =    GenreLengths. \
+#         map( lambda row: ( row[1][1][1],(row[1][1][0],1) ) ). \
+#         reduceByKey( lambda x,y: (x[0]+y[0] , x[1]+y[1]) ). \
+#         map( lambda row: ( row[0] , row[1][0]/row[1][1] ) ).collect()
 
 
+
+# for i in q4:
+#         print(i)
 
 
 
 
+# ---------------------------------------------                 Query 1 CSV               ------------------------------------------------
 
+def formatYear(timestamp):
+        return timestamp.split('-')[0]
 
+movies = spark.read.format("csv").options(header='false', inferSchema='true').load("hdfs://master:9000/data/movies.csv")
+
+movies.registerTempTable('movies')
+spark.udf.register("formatter", formatYear)
+
+# sqlQuery = "select YEAR(_c3) as year,MAX( (_c6+_c7)/_c7 ) as income , movies._c2 as movie from movies where YEAR(_c3)>2000 group by YEAR(_c3) order by YEAR(_c3) desc"
+# sqlQuery = """ 
+#         SELECT year(_c3), _c1, gross
+#         FROM movies
+#         WHERE year(_c3) IN (
+#                 SELECT year(_c3)
+#                 FROM movies
+#                 WHERE gross IN (
+#                         SELECT MAX( (_c6+_c7)/_c7 ) as gross
+#                         FROM movies
+#                         GROUP BY year(_c3)
+#                 )
+#         )
+# """
+
+sqlQuery = """SELECT year(_c3)
+                FROM movies
+                WHERE (_c6+_c7)/_c7 IN (
+                        SELECT MAX( (_c6+_c7)/_c7 )
+                        FROM movies
+                        GROUP BY year(_c3)
+                )"""
+# sqlQuery = "select * from movies"
+# sqlQuery = "SELECT EXTRACT(year from `_c3`) FROM movies"
+
+res = spark.sql(sqlQuery)
+
+res.show()
 
 
 
